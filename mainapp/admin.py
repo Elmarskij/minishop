734de500a7cf91ca575ab110b39b14549c6b510f
elmar.azimkulov <elmar.azimkulov@gmail.com>
2021-03-08 02:08:25 +0600

    Registration DB models of app "mainapp"

---
+++ mainapp/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+
+class ProductAdmin(admin.ModelAdmin):
+    change_form_template = 'mainapp/custom_admin/change_form.html'
+    #exclude = ('features',)
+
+
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(CartProduct)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Product)
